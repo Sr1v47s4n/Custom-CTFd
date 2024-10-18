44ce575dd0d2105c1d826a52180d1a3d65ced2db
Srivatsan Sk <117703051+Sr1v47s4n@users.noreply.github.com>
2024-10-18 23:11:11 +0530

    modified:   CTFd/utils/crypto/__init__.py

--- CTFd/utils/crypto/__init__.py
+++ CTFd/utils/crypto/__init__.py
@@ -1,19 +1,22 @@
+import bcrypt
 import hashlib
-
-from passlib.hash import bcrypt_sha256
-
 from CTFd.utils import string_types
-
-
-def hash_password(plaintext):
-    return bcrypt_sha256.hash(str(plaintext))
-
-
-def verify_password(plaintext, ciphertext):
-    return bcrypt_sha256.verify(plaintext, ciphertext)
 
 
 def sha256(p):
     if isinstance(p, string_types):
         p = p.encode("utf-8")
     return hashlib.sha256(p).hexdigest()
+
+
+def hash_password(plaintext):
+    # Hash the password with bcrypt, using a cost factor of 10
+    bcrypt_hashed = bcrypt.hashpw(plaintext.encode("utf-8"), bcrypt.gensalt(10))
+
+    # Return the bcrypt-hashed password as a UTF-8 string
+    return bcrypt_hashed.decode("utf-8")
+
+
+def verify_password(plaintext, hashed_password):
+    # Use bcrypt's checkpw to verify the password
+    return bcrypt.checkpw(plaintext.encode("utf-8"), hashed_password.encode("utf-8"))
